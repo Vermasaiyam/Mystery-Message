cd8f69e53c3c4ac0ce30fb90ea9274141d21d324
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-11 14:57:16 +0530

    change

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,5 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcryptjs from "bcryptjs";
+
